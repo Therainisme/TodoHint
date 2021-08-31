26e482a53ebbdefc5019d2aa10b511d07794cfb5
唤雨考拉 <therainisme@qq.com>
2021-08-31 17:12:34 +0800

    TS

--- test.ts
+++ test.ts
@@ -2,8 +2,11 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
 const arg: any = yargs(hideBin(process.argv)).argv;
+
 console.log(arg);
+
 if (arg.secret === "Hello") console.log("Yes!");
 else console.log("No");
+
 console.log('--------');
 console.log(process.argv);
